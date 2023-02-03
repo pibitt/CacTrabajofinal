f4373e47201ebe123a752cd166183a6105db1496
pibitt <pabloroquez@gmail.com>
2023-02-03 11:09:46 -0300

    fin clase 31

--- server.js
+++ server.js
@@ -1,38 +1,48 @@
 const express = require('express')
 const app = express()
-const hbs = require ('hbs');
+const hbs = require('hbs');
+const productos = require('./data/products.json');
 
 const port = 3000
 
 app.set('view engine', 'hbs');
-hbs.registerPartials(__dirname + '/views/partials', function (err) {});
+hbs.registerPartials(__dirname + '/views/partials', function (err) { });
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('index', {   //2 parametros, primero indica el nombre del archivo hbs, el segundo objeto)
-    titulo: "Mi página"
-  })
+    res.render('index', {   //2 parametros, primero indica el nombre del archivo hbs, el segundo objeto)
+        titulo: "Mi página",
+        productos: productos.products
+    })
 })
 
-app.get('/about', (req, res) => {
-  res.render('about', {   
-    titulo: "Sobre Nosotros"
-  })
+app.get('/como-comprar', (req, res) => {
+    res.render('como-comprar', {      
+        titulo: "Cómo comprar",     
+    })
 })
 
-// app.get('/shop', (req, res) => {
-//   res.render('shop', {
-//     titulo: "Tienda"
-//   })
-// })
+app.get('/contacto', (req, res) => {
+    res.render('contacto', {
+        titulo: "Contacto",         
+    })
+})
 
-// app.get('/contact', (req, res) => {
-//   res.render('contact', {
-//     titulo: "Contacto"
-//   })
-// })
+app.get('/producto-detalle', (req, res) => {
+    res.render('producto-detalle', {
+        titulo: "Detalla del producto", 
+    })
+})
+
+app.get('/sobre-nosotros', (req, res) => {
+    res.render('about', {
+        titulo: "Sobre Nosotros", 
+    })
+})
+
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
